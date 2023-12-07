59c937acb956a771d2aa2b078de4f33323f07c1c
Kristen Scheffler <kris10ben10@gmail.com>
2023-12-07 14:20:26 -0500

    showing stats

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,3 +1,69 @@
-public class VirtualPet{
+public class VirtualPet {
+    private String name;
+    private int hungerLevel = 50;
+    private int thirstLevel = 50;
+    private int boredLevel = 50;
 
+    public VirtualPet(String name) {
+        this.name = name;
+    }
+
+    public void feed() {
+        hungerLevel -= 10;
+        if (hungerLevel < 0) {
+            hungerLevel = 0;
+        }
+    }
+
+    public void giveDrink() {
+        thirstLevel -= 10;
+        if (thirstLevel < 0) {
+            thirstLevel = 0;
+        }
+    }
+
+    public void play() {
+        boredLevel -= 10;
+        if (boredLevel < 0) {
+            boredLevel = 0;
+        }
+    }
+
+    public void tick() {
+        hungerLevel += 5;
+        thirstLevel += 5;
+        boredLevel += 5;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHungerLevel() {
+        return hungerLevel;
+    }
+
+    public int getThirstLevel() {
+        return thirstLevel;
+    }
+
+    public int getBoredLevel() {
+        return boredLevel;
+    }
+
+    public void setBoredLevel(int boredLevel) {
+        this.boredLevel = boredLevel;
+    }
+
+    public void petStats() {
+        System.out.println("Name: " + this.name);
+        System.out.println("Hunger: " + this.hungerLevel);
+        System.out.println("Thirst: " + this.thirstLevel);
+        System.out.println("Bored Status: " + this.boredLevel);
+    }
+    
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,5 +1,56 @@
+import java.util.Scanner;
+
 public class VirtualPetApp {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Virtual Pet Game!");
+
+        System.out.print("Enter your pets name: ");
+        String name = scanner.nextLine();
+
+        VirtualPet pet = new VirtualPet(name);
+        int duration = 0;
+        int userChoice;
+
+        do {
+        System.out.println("\nDuration: " + duration);
+        pet.petStats();
+        System.out.println("What would you like to do?");
+        System.out.println("Enter 1 to feed " + pet.getName());
+        System.out.println("Enter 2 to give water to " + pet.getName());
+        System.out.println("Enter 3 to play with " + pet.getName());
+        System.out.println("Enter 4 to exit the game");
+        System.out.print("\nEnter your choice: ");
+
+        userChoice = scanner.nextInt();
+
+        switch (userChoice) {
+            case 1:
+                System.out.println("You fed your pet!");
+                pet.feed();
+                break;
+            case 2:
+                System.out.println("You gave water to your pet!");
+                pet.giveDrink();
+                break;
+            case 3:
+                System.out.println("You played with your pet!");
+                pet.play();
+                break;
+            case 4:
+                System.out.println("Good bye, see you next time!");
+                break;
+            default:
+            System.out.println("Invalid choice, please enter 1, 2, 3 or 4!");
+                break;
+        }
+
+        pet.tick();
+        duration++;
+
+        } while (userChoice != 4);
+
+        scanner.close();
         
     }
 }
